7981d01662d7f8ba460181996e3245b9c2069b40
Xrenore3 <romanchaus519@gmail.com>
2020-10-22 19:40:33 +0300

    deploy project

--- src/App.js
+++ src/App.js
@@ -2,7 +2,23 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [people,setPeople] = useState(data);
+  const clearSelectedUser = (id) => {
+    const newPeople = people.filter(person=>{
+      if (person.id !== id){
+        return person
+      }
+    })
+     setPeople(newPeople)
+  }
+  return <main>
+    <section className='container'>
+      <h3>{people.length} birthday today</h3>
+      <List people={people} clearSelectedUser={clearSelectedUser}/>
+      <button onClick={()=>setPeople([])}>Clear All</button>
+
+    </section>
+  </main>;
 }
 
 export default App;
--- src/List.js
+++ src/List.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React from "react";
 
-const List = () => {
-  return (
-    <>
-      <h2>list component</h2>
-    </>
-  );
+const List = (props) => {
+  return props.people.map((person) => {
+    const { id, name, age, image } = person;
+    return (
+      <article key={id} className="person">
+        <img src={image} alt={name} />
+        <div>
+          <h4>{name}</h4>
+          <p>{age}</p>
+          <button onClick={()=>props.clearSelectedUser(id)}>remove</button>
+        </div>
+      </article>
+    );
+  });
 };
 
 export default List;
